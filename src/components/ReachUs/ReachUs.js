01398dd7f9bd0a3c4e34ad9ea901aa5d8dfd3b39
Jack <jk19011999@gmail.com>
2023-09-20 20:37:38 +0530

    Added save comments flow

--- src/components/ReachUs/ReachUs.js
+++ src/components/ReachUs/ReachUs.js
@@ -1,9 +1,31 @@
 import './reachus.css'
-import React from 'react'
+import backendService from '../../services/backendService'
+import React, { useState } from 'react'
 
 import { FaGithub, FaLinkedin, FaInstagram, FaYoutube, FaCopyright } from 'react-icons/fa'
 
 export default function ReachUs() {
+
+    const [email, setEmail] = useState('');
+    const [comment, setComment] = useState('');
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const commentObject = {
+            email: email,
+            comments: comment,
+            replied: false,
+            message: null,
+        };
+        const result = await backendService.getCommentsData(commentObject);
+        if (result.message === 'Request Processed') {
+            setEmail('');
+            setComment('');
+            // TODO: show banner as successfully sent comment
+        }
+    }
+
+
     return (
         <div className='main_reachus_container pt-5'>
             <div className='row'>
@@ -36,17 +58,17 @@ export default function ReachUs() {
                 <div className='col-6 sub_right_reachus_container'>
                     <div className='container container_contact_form'>
                         <div className="wrapper">
-                            <form>
+                            <form onSubmit={handleSubmit}>
                                 <h1>Contact me</h1>
                                 <div className='mb-3 input-box'>
-                                    <input type="email" placeholder="Email" required />
+                                    <input type="email" placeholder="Email" value={email} onChange={event => setEmail(event.target.value)} required />
                                     <i className='bx bxs-user'></i>
                                 </div>
                                 <div className='mb-3 form-floating input-box'>
-                                    <textarea placeholder="Write your comment" required></textarea>
+                                    <textarea placeholder="Write your comment" value={comment} onChange={event => setComment(event.target.value)} required></textarea>
                                     <i className='bx bxs-user'></i>
                                 </div>
-                                <button type="button" className='btn btn-outline-light button_submit'>
+                                <button type="submit" className='btn btn-outline-light button_submit'>
                                     Submit
                                 </button>
                             </form>
